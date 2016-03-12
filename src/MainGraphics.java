a09a86382e037c1177924e34532602f0de6a20de
umarusaf <umarusaf902@gmail.com>
2016-03-13 00:48:33 +0500

    Umar's Edited1

--- src/MainGraphics.java
+++ src/MainGraphics.java
@@ -18,6 +18,7 @@ public class MainGraphics {
 		   String sec=null;
 		   
 		   System.out.println("Who will Start First \n0- You\n1- Transformer\nOption : ");
+		System.out.println("Umar's Edited ");
 		   int turn = inp.nextInt();
 		   
 		   if(turn==0){
